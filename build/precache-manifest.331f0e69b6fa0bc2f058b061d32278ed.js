e414f9af24ab08cc015c371bb3b48dadfdf66829
mario <mario496luisin@gmail.com>
2019-03-22 00:19:58 +0000

    Cotizador de prestamos React

---
+++ build/precache-manifest.331f0e69b6fa0bc2f058b061d32278ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a6912725c813d53f3321",
+    "url": "/static/js/main.a6912725.chunk.js"
+  },
+  {
+    "revision": "7c07c4421856b5076fcb",
+    "url": "/static/js/2.7c07c442.chunk.js"
+  },
+  {
+    "revision": "a6912725c813d53f3321",
+    "url": "/static/css/main.9c98d13a.chunk.css"
+  },
+  {
+    "revision": "95156816e66b68e21699fde93fbe9bef",
+    "url": "/index.html"
+  }
+];
